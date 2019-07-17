9a9022072dc9291bccdbc2d712c2b8f86478cc45
yurikbv <yurikbvv@gmail.com>
2019-07-18 01:24:29 +0300

    finished login section

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const { findOrCreateUser } = require('./controllers/userController');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true})
     .then(() => console.log('DB connected!'))
@@ -11,7 +12,22 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true})
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: async ({req}) => {
+    let authToken = null;
+    let currentUser = null;
+
+    try {
+      authToken = req.headers.authorization;
+      if (authToken) {
+        currentUser = await findOrCreateUser(authToken);
+      }
+    } catch (e) {
+      console.error(`Unable to authenticate user with token ${authToken}`)
+    }
+
+    return {currentUser}
+  }
 });
 
 server.listen().then(({url}) => {
