c267a70e0cf634f5faeb6345c6a6939bdecbcc47
yurikbv <yurikbvv@gmail.com>
2019-10-04 01:14:11 +0300

    deploy

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true})
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  introspection: true,
+  playground: true,
   context: async ({req}) => {
     let authToken = null;
     let currentUser = null;
